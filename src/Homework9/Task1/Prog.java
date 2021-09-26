573ac9930ff1502ea638e0c8c8373d477df4a1d9
TaBletKa97 <mtabletka.by@gmail.com>
2021-09-26 22:56:47 +0300

    player

---
+++ src/Homework9/Task1/Prog.java
@@ -0,0 +1,11 @@
+package Homework9.Task1;
+
+public class Prog {
+    public static void main(String[] args) {
+        Core.getGames().add("Dota 2");
+        Core.getGames().add("CS:GO");
+        Core.getGames().add("PUBG");
+        Core.getGames().add("LoL");
+        Core.showGames();
+    }
+}
